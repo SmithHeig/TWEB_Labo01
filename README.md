a8e1ba6050f620605cbfad6570968e0d9bc4f0ff
James <james.smith@heig-vd.ch>
2018-09-21 11:44:48 +0200

    widget profile

---
+++ README.md
@@ -0,0 +1,3 @@
+# Teaching-TWEB-2018-Basic-HTML-Example
+Basic web page that contains HTML, CSS and a little bit of JS.  
+Juste open the "index.html" file in order to run it locally.
---
+++ js/script.js
@@ -0,0 +1,7 @@
+function moveFunny() {
+  document.getElementById('button-subscribe').className = "button-subscribe-over";
+}
+
+function subscribe() {
+ alert("YOU SHOULDN'T DO THAT !!!! YOU FOOL!!!");
+}
